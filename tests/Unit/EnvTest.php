697eaa36a035dcfbbada8bd6be224a5d4518b04a
devuri <devuri@users.noreply.github.com>
2024-02-19 01:38:02 -0500

    chore: build

--- tests/Unit/EnvTest.php
+++ tests/Unit/EnvTest.php
@@ -2,10 +2,15 @@
 
 namespace Tests\Unit;
 
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 use Urisoft\Env;
-use InvalidArgumentException;
 
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
 class EnvTest extends TestCase
 {
     protected $env;
@@ -25,34 +30,34 @@ class EnvTest extends TestCase
         $_ENV['BOOL_FALSE'] = 'false';
     }
 
-    public function testGetExistingVariable()
+    public function test_get_existing_variable(): void
     {
         $this->assertSame('test value', $this->env->get('TEST_VAR'));
     }
 
-    public function testGetNonExistingVariableReturnsDefault()
+    public function test_get_non_existing_variable_returns_default(): void
     {
         $this->assertSame('default', $this->env->get('NON_EXISTING', 'default'));
     }
 
-    public function testGetIntegerVariable()
+    public function test_get_integer_variable(): void
     {
         $this->assertSame(123, $this->env->get('INT_VAR'));
     }
 
-    public function testGetBooleanTrueVariable()
+    public function test_get_boolean_true_variable(): void
     {
         $this->assertTrue($this->env->get('BOOL_TRUE'));
     }
 
-    public function testGetBooleanFalseVariable()
+    public function test_get_boolean_false_variable(): void
     {
         $this->assertFalse($this->env->get('BOOL_FALSE'));
     }
 
-	public function testAccessToNonWhitelistedVariableThrowsException()
-	{
-	    $this->expectException(InvalidArgumentException::class);
-	    $this->env->get('NON_EXISTING_VAR');
-	}
+    public function test_access_to_non_whitelisted_variable_throws_exception(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->env->get('NON_EXISTING_VAR');
+    }
 }
