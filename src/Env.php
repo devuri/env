ca5d05ad7e70d3a5d4e07b023e9a4b03e3d28c18
devuri <devuri@users.noreply.github.com>
2024-02-19 18:43:34 -0500

    chore: build

--- src/Env.php
+++ src/Env.php
@@ -20,73 +20,51 @@ class Env
         $this->whitelist = $whitelist;
     }
 
-	/**
-	 * Sanitizes a given string by removing or encoding unwanted characters and whitespace.
-	 *
-	 * This method serves as a public interface to the `_sanitizeIt` method, providing
-	 * a way to sanitize input strings according to the specified level of strictness.
-	 * When `$strict` is true, it employs a more rigorous sanitization process by stripping
-	 * all HTML tags from the input.
-	 *
-	 * @param string $value The input string to be sanitized.
-	 * @param bool $strict Optional. Determines the sanitization level. When set to true,
-	 *                     the method performs a more stringent sanitization by removing
-	 *                     all HTML tags. Defaults to false, in which case the input string
-	 *                     is sanitized without removing HTML tags.
-	 * @return string The sanitized string.
-	 */
-	public function sanitize(string $value, bool $strict = false): string
-	{
-	    return $this->_sanitizeIt($value, $strict);
-	}
+    /**
+     * Sanitizes a given string by removing or encoding unwanted characters and whitespace.
+     *
+     * This method serves as a public interface to the `_sanitizeIt` method, providing
+     * a way to sanitize input strings according to the specified level of strictness.
+     * When `$strict` is true, it employs a more rigorous sanitization process by stripping
+     * all HTML tags from the input.
+     *
+     * @param string $value  The input string to be sanitized.
+     * @param bool   $strict Optional. Determines the sanitization level. When set to true,
+     *                       the method performs a more stringent sanitization by removing
+     *                       all HTML tags. Defaults to false, in which case the input string
+     *                       is sanitized without removing HTML tags.
+     *
+     * @return string The sanitized string.
+     */
+    public function sanitize( string $value, bool $strict = false ): string
+    {
+        return $this->_sanitizeIt( $value, $strict );
+    }
 
-	/**
-	 * Performs the actual sanitization of a given string based on the specified level of strictness.
-	 *
-	 * This private method is responsible for the core sanitization logic applied to the input string.
-	 * It trims whitespace from the beginning and end of the string, optionally strips all HTML tags
-	 * if strict sanitization is required, and removes excessive whitespace and non-printable characters
-	 * from the string to ensure it is clean and safe for use.
-	 *
-	 * @param string $value The input string to be sanitized.
-	 * @param bool $strict Determines the sanitization level. When set to true, the method
-	 *                     strips all HTML tags from the input string for a stricter sanitization.
-	 *                     Defaults to false, allowing HTML tags to remain in the sanitized output.
-	 * @return string The sanitized string, with unwanted characters and excessive whitespace removed.
-	 */
-	private function _sanitizeIt(string $value, bool $strict = false): string
-	{
-	    $value = trim($value);
-	    $value = $strict ? strip_tags($value) : $value;
-	    $value = preg_replace('/[\r\n\t ]+/', ' ', $value);
-	    $value = preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]+/', '', $value);
-	    return $value;
-	}
-
-	/**
-	 * Retrieves a sanitized, and optionally encrypted or modified, environment variable by name.
-	 *
-	 * This method first checks if the requested environment variable name is within the allowed whitelist.
-	 * If not, it throws an InvalidArgumentException. If the variable is in the whitelist, it retrieves the value
-	 * from the environment, with a fallback to the provided default or encryption flag. The value is then sanitized
-	 * for safe use. If encryption is requested (by setting the second parameter to true), and an encryption path
-	 * is defined, the value is encrypted. Otherwise, the method attempts to cast the value to its appropriate
-	 * data type based on its content. It supports casting to integer and boolean, and recognizes 'null' values
-	 * which are converted to an empty string. Additionally, the value can be converted to lowercase based on
-	 * the `$strtolower` parameter.
-	 *
-	 * @param string $name             The name of the environment variable to retrieve.
-	 * @param mixed  $defaultOrEncrypt Default value to return if the environment variable is not set,
-	 *                                 or boolean true to indicate that the value should be encrypted.
-	 * @param bool   $strtolower       Whether to convert the retrieved value to lowercase. Defaults to false.
-	 *
-	 * @throws InvalidArgumentException If the requested environment variable name is not in the whitelist
-	 *                                  or if encryption is requested but the encryption path is not defined.
-	 *
-	 * @return mixed The sanitized environment variable value, possibly encrypted or typecast,
-	 *               or transformed to lowercase if specified.
-	 */
-	public function get( $name, $defaultOrEncrypt = null, $strtolower = false )
+    /**
+     * Retrieves a sanitized, and optionally encrypted or modified, environment variable by name.
+     *
+     * This method first checks if the requested environment variable name is within the allowed whitelist.
+     * If not, it throws an InvalidArgumentException. If the variable is in the whitelist, it retrieves the value
+     * from the environment, with a fallback to the provided default or encryption flag. The value is then sanitized
+     * for safe use. If encryption is requested (by setting the second parameter to true), and an encryption path
+     * is defined, the value is encrypted. Otherwise, the method attempts to cast the value to its appropriate
+     * data type based on its content. It supports casting to integer and boolean, and recognizes 'null' values
+     * which are converted to an empty string. Additionally, the value can be converted to lowercase based on
+     * the `$strtolower` parameter.
+     *
+     * @param string $name             The name of the environment variable to retrieve.
+     * @param mixed  $defaultOrEncrypt Default value to return if the environment variable is not set,
+     *                                 or boolean true to indicate that the value should be encrypted.
+     * @param bool   $strtolower       Whether to convert the retrieved value to lowercase. Defaults to false.
+     *
+     * @throws InvalidArgumentException If the requested environment variable name is not in the whitelist
+     *                                  or if encryption is requested but the encryption path is not defined.
+     *
+     * @return mixed The sanitized environment variable value, possibly encrypted or typecast,
+     *               or transformed to lowercase if specified.
+     */
+    public function get( $name, $defaultOrEncrypt = null, $strtolower = false )
     {
         if ( ! \in_array( $name, $this->whitelist, true ) ) {
             throw new InvalidArgumentException( "Access to undefined environment variable: {$name}" );
@@ -94,8 +72,8 @@ class Env
 
         $value = $_ENV[ $name ] ?? $defaultOrEncrypt;
 
-		// Sanitize the value.
-	    $value = $this->sanitize($value);
+        // Sanitize the value.
+        $value = $this->sanitize( $value );
 
         if ( true === $defaultOrEncrypt ) {
             if ( ! $this->encryptionPath ) {
@@ -140,5 +118,29 @@ class Env
     protected function isIntVal( $str )
     {
         return is_numeric( $str ) && \intval( $str ) == $str;
+    }
+
+    /**
+     * Performs the actual sanitization of a given string based on the specified level of strictness.
+     *
+     * This private method is responsible for the core sanitization logic applied to the input string.
+     * It trims whitespace from the beginning and end of the string, optionally strips all HTML tags
+     * if strict sanitization is required, and removes excessive whitespace and non-printable characters
+     * from the string to ensure it is clean and safe for use.
+     *
+     * @param string $value  The input string to be sanitized.
+     * @param bool   $strict Determines the sanitization level. When set to true, the method
+     *                       strips all HTML tags from the input string for a stricter sanitization.
+     *                       Defaults to false, allowing HTML tags to remain in the sanitized output.
+     *
+     * @return string The sanitized string, with unwanted characters and excessive whitespace removed.
+     */
+    private function _sanitizeIt( string $value, bool $strict = false ): string
+    {
+        $value = trim( $value );
+        $value = $strict ? strip_tags( $value ) : $value;
+        $value = preg_replace( '/[\r\n\t ]+/', ' ', $value );
+
+        return preg_replace( '/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]+/', '', $value );
     }
 }
--- tests/Unit/SecurityKeysTest.php
+++ tests/Unit/SecurityKeysTest.php
@@ -5,6 +5,11 @@ namespace Tests\Unit;
 use PHPUnit\Framework\TestCase;
 use Urisoft\Env;
 
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
 class SecurityKeysTest extends TestCase
 {
     protected $env;
@@ -13,75 +18,75 @@ class SecurityKeysTest extends TestCase
     {
         parent::setUp();
 
-		$this->env = new Env( [
-		    'AUTH_KEY',
-		    'SECURE_AUTH_KEY',
-		    'LOGGED_IN_KEY',
-		    'NONCE_KEY',
-		    'AUTH_SALT',
-		    'SECURE_AUTH_SALT',
-		    'LOGGED_IN_SALT',
-		    'NONCE_SALT'
-		]);
+        $this->env = new Env( [
+            'AUTH_KEY',
+            'SECURE_AUTH_KEY',
+            'LOGGED_IN_KEY',
+            'NONCE_KEY',
+            'AUTH_SALT',
+            'SECURE_AUTH_SALT',
+            'LOGGED_IN_SALT',
+            'NONCE_SALT',
+        ]);
 
-		$security_keys = [
-		    'AUTH_KEY' => 'BmR+ZqO+%U5J`(/x<`k6<80P`yJ;ZPo~wo=IiVi7&9MucFW~2HVl?h^x|4=cGN<)',
-		    'SECURE_AUTH_KEY' => '`4V+fJoIn-3CO7h1M1EVNwMg-aGGhhHb59Qt+ZzIsuR5}qg3@-A)y|)0m+|k+1`K',
-		    'LOGGED_IN_KEY' => 'P6CyuyB~|oaA.V>yVnl@5W@aA7ppYdBR-_WnYh~FQ^w.#`6,,V[*:$h!:5umXsW%',
-		    'NONCE_KEY' => 'dL{$#(o.z|Q>!z=>Jel*&J5|Jzn1;Qm7`N^ME4lE(-v)7-{G2Hiw|3baK<Q#.gZg',
-		    'AUTH_SALT' => '.*gOx0.,m[,yIWQ[ NL1|6-+/yu5Z~?Y8mDRrIuahI!G)>K|5j,UIp+%|WNgr4W;',
-		    'SECURE_AUTH_SALT' => 'hH8TjIPb(`y,FP/ 5&luAxnG>fN?ndXTX:vXLWI|S9r0<z;OE8+e|mad)ywlq%(E',
-		    'LOGGED_IN_SALT' => 'K#8g${uHqg>P>ZKb*|lJ(7j1].gF}}w$buA-*J@++/=F#K[&)nQeB~oyXApBpLJf',
-		    'NONCE_SALT' => 'p^:mVp7ww-ynCm[j;y3:Zt`YB{GW5x2@HopGKpmz%;Dk/}sY)izWA/+Gn_2BR.JW'
-		];
+        $security_keys = [
+            'AUTH_KEY' => 'BmR+ZqO+%U5J`(/x<`k6<80P`yJ;ZPo~wo=IiVi7&9MucFW~2HVl?h^x|4=cGN<)',
+            'SECURE_AUTH_KEY' => '`4V+fJoIn-3CO7h1M1EVNwMg-aGGhhHb59Qt+ZzIsuR5}qg3@-A)y|)0m+|k+1`K',
+            'LOGGED_IN_KEY' => 'P6CyuyB~|oaA.V>yVnl@5W@aA7ppYdBR-_WnYh~FQ^w.#`6,,V[*:$h!:5umXsW%',
+            'NONCE_KEY' => 'dL{$#(o.z|Q>!z=>Jel*&J5|Jzn1;Qm7`N^ME4lE(-v)7-{G2Hiw|3baK<Q#.gZg',
+            'AUTH_SALT' => '.*gOx0.,m[,yIWQ[ NL1|6-+/yu5Z~?Y8mDRrIuahI!G)>K|5j,UIp+%|WNgr4W;',
+            'SECURE_AUTH_SALT' => 'hH8TjIPb(`y,FP/ 5&luAxnG>fN?ndXTX:vXLWI|S9r0<z;OE8+e|mad)ywlq%(E',
+            'LOGGED_IN_SALT' => 'K#8g${uHqg>P>ZKb*|lJ(7j1].gF}}w$buA-*J@++/=F#K[&)nQeB~oyXApBpLJf',
+            'NONCE_SALT' => 'p^:mVp7ww-ynCm[j;y3:Zt`YB{GW5x2@HopGKpmz%;Dk/}sY)izWA/+Gn_2BR.JW',
+        ];
 
-		// Set environment variables for testing
-		$_ENV['AUTH_KEY'] = $security_keys['AUTH_KEY'];
-		$_ENV['SECURE_AUTH_KEY'] = $security_keys['SECURE_AUTH_KEY'];
-		$_ENV['LOGGED_IN_KEY'] = $security_keys['LOGGED_IN_KEY'];
-		$_ENV['NONCE_KEY'] = $security_keys['NONCE_KEY'];
-		$_ENV['AUTH_SALT'] = $security_keys['AUTH_SALT'];
-		$_ENV['SECURE_AUTH_SALT'] = $security_keys['SECURE_AUTH_SALT'];
-		$_ENV['LOGGED_IN_SALT'] = $security_keys['LOGGED_IN_SALT'];
-		$_ENV['NONCE_SALT'] = $security_keys['NONCE_SALT'];
+        // Set environment variables for testing
+        $_ENV['AUTH_KEY'] = $security_keys['AUTH_KEY'];
+        $_ENV['SECURE_AUTH_KEY'] = $security_keys['SECURE_AUTH_KEY'];
+        $_ENV['LOGGED_IN_KEY'] = $security_keys['LOGGED_IN_KEY'];
+        $_ENV['NONCE_KEY'] = $security_keys['NONCE_KEY'];
+        $_ENV['AUTH_SALT'] = $security_keys['AUTH_SALT'];
+        $_ENV['SECURE_AUTH_SALT'] = $security_keys['SECURE_AUTH_SALT'];
+        $_ENV['LOGGED_IN_SALT'] = $security_keys['LOGGED_IN_SALT'];
+        $_ENV['NONCE_SALT'] = $security_keys['NONCE_SALT'];
     }
 
-    public function testAuthKey()
+    public function test_auth_key(): void
     {
         $this->assertSame('BmR+ZqO+%U5J`(/x<`k6<80P`yJ;ZPo~wo=IiVi7&9MucFW~2HVl?h^x|4=cGN<)', $this->env->get('AUTH_KEY'));
     }
 
-    public function testSecureAuthKey()
+    public function test_secure_auth_key(): void
     {
         $this->assertSame('`4V+fJoIn-3CO7h1M1EVNwMg-aGGhhHb59Qt+ZzIsuR5}qg3@-A)y|)0m+|k+1`K', $this->env->get('SECURE_AUTH_KEY'));
     }
 
-    public function testLoggedInKey()
+    public function test_logged_in_key(): void
     {
         $this->assertSame('P6CyuyB~|oaA.V>yVnl@5W@aA7ppYdBR-_WnYh~FQ^w.#`6,,V[*:$h!:5umXsW%', $this->env->get('LOGGED_IN_KEY'));
     }
 
-    public function testNonceKey()
+    public function test_nonce_key(): void
     {
         $this->assertSame('dL{$#(o.z|Q>!z=>Jel*&J5|Jzn1;Qm7`N^ME4lE(-v)7-{G2Hiw|3baK<Q#.gZg', $this->env->get('NONCE_KEY'));
     }
 
-    public function testAuthSalt()
+    public function test_auth_salt(): void
     {
         $this->assertSame('.*gOx0.,m[,yIWQ[ NL1|6-+/yu5Z~?Y8mDRrIuahI!G)>K|5j,UIp+%|WNgr4W;', $this->env->get('AUTH_SALT'));
     }
 
-    public function testSecureAuthSalt()
+    public function test_secure_auth_salt(): void
     {
         $this->assertSame('hH8TjIPb(`y,FP/ 5&luAxnG>fN?ndXTX:vXLWI|S9r0<z;OE8+e|mad)ywlq%(E', $this->env->get('SECURE_AUTH_SALT'));
     }
 
-    public function testLoggedInSalt()
+    public function test_logged_in_salt(): void
     {
         $this->assertSame('K#8g${uHqg>P>ZKb*|lJ(7j1].gF}}w$buA-*J@++/=F#K[&)nQeB~oyXApBpLJf', $this->env->get('LOGGED_IN_SALT'));
     }
 
-    public function testNonceSalt()
+    public function test_nonce_salt(): void
     {
         $this->assertSame('p^:mVp7ww-ynCm[j;y3:Zt`YB{GW5x2@HopGKpmz%;Dk/}sY)izWA/+Gn_2BR.JW', $this->env->get('NONCE_SALT'));
     }
